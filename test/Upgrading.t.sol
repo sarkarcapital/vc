efaf8fa6e313ad4c48ae34878b185d4ec79befeb
Jør∂¡ <4456749+brickpop@users.noreply.github.com>
2025-06-25 15:10:37 +0200

    Build 3 is not relevant for upgrades (yet)

--- test/Upgrading.t.sol
+++ test/Upgrading.t.sol
@@ -144,18 +144,4 @@ contract UpgradingTest is Test {
         // It the original `initialize` function is disabled post-upgrade
         vm.skip(true);
     }
-
-    modifier givenTheContractIsAtR1B3() {
-        _;
-    }
-
-    function test_WhenUpgradingWithInitializeFrom3() external givenTheContractIsAtR1B3 {
-        // It upgrades from R1 B3 with `initializeFrom`
-        // It The old `initialize` function fails during the upgrade
-        // It initializeFrom succeeds
-        // It protocol versions are updated correctly
-        // It new settings are applied
-        // It the original `initialize` function is disabled post-upgrade
-        vm.skip(true);
-    }
 }
