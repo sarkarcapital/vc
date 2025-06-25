5d406f20fcf117636e2fc4c72e1491c23c42e5a1
Jør∂¡ <4456749+brickpop@users.noreply.github.com>
2025-06-25 21:44:07 +0200

    Adding Plugin Setup fork tests

--- test/fork-tests/PluginSetupFork.t.sol
+++ test/fork-tests/PluginSetupFork.t.sol
@@ -1,9 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
-import {Test} from "forge-std/Test.sol";
+import {ForkTestBase} from "../lib/ForkTestBase.sol";
 
-contract PluginSetupForkTest is Test {
+import {ForkBuilder} from "../builders/ForkBuilder.sol";
+import {DAO} from "@aragon/osx/core/dao/DAO.sol";
+import {DaoUnauthorized} from "@aragon/osx-commons-contracts/src/permission/auth/auth.sol";
+import {PluginRepo} from "@aragon/osx/framework/plugin/repo/PluginRepo.sol";
+
+import {TokenVotingSetup} from "../../src/TokenVotingSetup.sol";
+import {TokenVoting} from "../../src/TokenVoting.sol";
+import {NON_EMPTY_BYTES} from "../constants.sol";
+
+import {ForkTestBase} from "../lib/ForkTestBase.sol";
+import {ForkBuilder} from "../builders/ForkBuilder.sol";
+
+// Aragon OSx Contracts
+import {DAO} from "@aragon/osx/core/dao/DAO.sol";
+import {PluginRepo} from "@aragon/osx/framework/plugin/repo/PluginRepo.sol";
+import {
+    PluginSetupRef,
+    hashHelpers,
+    hashPermissions
+} from "@aragon/osx/framework/plugin/setup/PluginSetupProcessorHelpers.sol";
+import {PluginSetupProcessor} from "@aragon/osx/framework/plugin/setup/PluginSetupProcessor.sol";
+import {IPluginSetup} from "@aragon/osx-commons-contracts/src/plugin/setup/IPluginSetup.sol";
+import {IPlugin} from "@aragon/osx-commons-contracts/src/plugin/IPlugin.sol";
+import {PermissionLib} from "@aragon/osx-commons-contracts/src/permission/PermissionLib.sol";
+
+// Plugin Contracts
+import {MajorityVotingBase} from "../../src/base/MajorityVotingBase.sol";
+import {GovernanceERC20} from "../../src/erc20/GovernanceERC20.sol";
+
+// OZ Contracts
+import {IVotesUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
+
+contract PluginSetupForkTest is ForkTestBase {
+    // An address of the official TokenVoting plugin repository on Sepolia
+    // This allows testing updates from previously published builds
+    // https://sepolia.etherscan.io/address/0x481633515A23374251a84497e335222f5a435A91
+    address private constant TOKEN_VOTING_REPO_ADDRESS = 0x481633515a23374251A84497e335222F5A435a91;
+
+    // Actors
+    address private deployer = address(this);
+
     modifier givenTheDeployerHasAllNecessaryPermissionsForInstallationAndUninstallation() {
         _;
     }
@@ -13,7 +53,100 @@ contract PluginSetupForkTest is Test {
         givenTheDeployerHasAllNecessaryPermissionsForInstallationAndUninstallation
     {
         // It installs & uninstalls the current build with a token
-        vm.skip(true);
+
+        // SETUP: Create a DAO and an "existing" token for it to use
+        (DAO dao, PluginRepo repo,,) = new ForkBuilder().build();
+        GovernanceERC20 existingToken;
+        {
+            address[] memory receivers = new address[](1);
+            receivers[0] = alice;
+            uint256[] memory amounts = new uint256[](1);
+            amounts[0] = 100 ether;
+            GovernanceERC20.MintSettings memory initialMint = GovernanceERC20.MintSettings(receivers, amounts);
+            existingToken = new GovernanceERC20(dao, "Existing Token", "EXIST", initialMint);
+        }
+
+        // PERMISSIONS: Grant the necessary permissions to the PluginSetupProcessor
+
+        dao.grant(address(pluginSetupProcessor), deployer, pluginSetupProcessor.APPLY_INSTALLATION_PERMISSION_ID());
+        dao.grant(address(pluginSetupProcessor), deployer, pluginSetupProcessor.APPLY_UNINSTALLATION_PERMISSION_ID());
+        dao.grant(address(dao), address(pluginSetupProcessor), dao.ROOT_PERMISSION_ID());
+
+        PluginSetupRef memory setupRef = PluginSetupRef({versionTag: getLatestTag(repo), pluginSetupRepo: repo});
+
+        // INSTALLATION
+        // Prepare installation data using the existing token
+        bytes memory installData;
+        {
+            MajorityVotingBase.VotingSettings memory votingSettings = MajorityVotingBase.VotingSettings(
+                MajorityVotingBase.VotingMode.EarlyExecution, 500_000, 200_000, 1 hours, 0
+            );
+            TokenVotingSetup.TokenSettings memory tokenSettings =
+                TokenVotingSetup.TokenSettings({addr: address(existingToken), name: "Test Token", symbol: "TTK"});
+            GovernanceERC20.MintSettings memory emptyMintSettings =
+                GovernanceERC20.MintSettings(new address[](0), new uint256[](0));
+            IPlugin.TargetConfig memory targetConfig = IPlugin.TargetConfig(address(dao), IPlugin.Operation.Call);
+
+            installData = abi.encode(votingSettings, tokenSettings, emptyMintSettings, targetConfig, 0, "some-metadata");
+        }
+
+        // Prepare and apply the installation
+        address plugin;
+        IPluginSetup.PreparedSetupData memory preparedSetupData;
+        {
+            PluginSetupProcessor.PrepareInstallationParams memory prepareInstallParams =
+                PluginSetupProcessor.PrepareInstallationParams({pluginSetupRef: setupRef, data: installData});
+
+            (plugin, preparedSetupData) = pluginSetupProcessor.prepareInstallation(address(dao), prepareInstallParams);
+
+            PluginSetupProcessor.ApplyInstallationParams memory applyInstallParams = PluginSetupProcessor
+                .ApplyInstallationParams({
+                pluginSetupRef: setupRef,
+                plugin: plugin,
+                permissions: preparedSetupData.permissions,
+                helpersHash: hashHelpers(preparedSetupData.helpers)
+            });
+            pluginSetupProcessor.applyInstallation(plugin, applyInstallParams);
+
+            vm.assertTrue(
+                dao.isGranted(address(dao), address(plugin), dao.EXECUTE_PERMISSION_ID(), ""),
+                "Plugin should be installed"
+            );
+        }
+
+        // Assert that the installation was successful and configured correctly
+        {
+            TokenVoting tokenVotingPlugin = TokenVoting(plugin);
+            assertEq(address(tokenVotingPlugin.getVotingToken()), address(existingToken), "Token address mismatch");
+            assertTrue(tokenVotingPlugin.isMember(alice), "Alice should be a member");
+            assertFalse(tokenVotingPlugin.isMember(bob), "Bob should not be a member");
+        }
+
+        // UNINSTALLATION
+        // Prepare and apply the uninstallation
+        {
+            PluginSetupProcessor.PrepareUninstallationParams memory uninstallParams = PluginSetupProcessor
+                .PrepareUninstallationParams({
+                pluginSetupRef: setupRef,
+                setupPayload: IPluginSetup.SetupPayload({
+                    plugin: plugin,
+                    currentHelpers: preparedSetupData.helpers,
+                    data: ""
+                })
+            });
+            PermissionLib.MultiTargetPermission[] memory uninstallPermissions =
+                pluginSetupProcessor.prepareUninstallation(address(dao), uninstallParams);
+
+            PluginSetupProcessor.ApplyUninstallationParams memory applyUninstallParams = PluginSetupProcessor
+                .ApplyUninstallationParams({plugin: plugin, pluginSetupRef: setupRef, permissions: uninstallPermissions});
+            pluginSetupProcessor.applyUninstallation(plugin, applyUninstallParams);
+        }
+
+        // Assert that the plugin was successfully uninstalled
+        vm.assertFalse(
+            dao.isGranted(address(dao), address(plugin), dao.EXECUTE_PERMISSION_ID(), ""),
+            "Plugin should be uninstalled"
+        );
     }
 
     modifier givenTheDeployerHasAllNecessaryPermissionsForInstallationAndUninstallation2() {
@@ -25,30 +158,96 @@ contract PluginSetupForkTest is Test {
         givenTheDeployerHasAllNecessaryPermissionsForInstallationAndUninstallation2
     {
         // It installs & uninstalls the current build without a token
-        vm.skip(true);
-    }
 
-    modifier givenAPreviousPluginBuild1IsInstalledAndTheDeployerHasUpdatePermissions() {
-        _;
-    }
+        // SETUP: Create a DAO
+        (DAO dao, PluginRepo repo,,) = new ForkBuilder().build();
 
-    function test_WhenUpdatingFromBuild1ToTheCurrentBuild()
-        external
-        givenAPreviousPluginBuild1IsInstalledAndTheDeployerHasUpdatePermissions
-    {
-        // It updates from build 1 to the current build
-        vm.skip(true);
-    }
+        // PERMISSIONS: Grant the necessary permissions
 
-    modifier givenAPreviousPluginBuild2IsInstalledAndTheDeployerHasUpdatePermissions() {
-        _;
-    }
+        dao.grant(address(pluginSetupProcessor), deployer, pluginSetupProcessor.APPLY_INSTALLATION_PERMISSION_ID());
+        dao.grant(address(pluginSetupProcessor), deployer, pluginSetupProcessor.APPLY_UNINSTALLATION_PERMISSION_ID());
+        dao.grant(address(dao), address(pluginSetupProcessor), dao.ROOT_PERMISSION_ID());
 
-    function test_WhenUpdatingFromBuild2ToTheCurrentBuild()
-        external
-        givenAPreviousPluginBuild2IsInstalledAndTheDeployerHasUpdatePermissions
-    {
-        // It updates from build 2 to the current build
-        vm.skip(true);
+        PluginSetupRef memory setupRef = PluginSetupRef({versionTag: getLatestTag(repo), pluginSetupRepo: repo});
+
+        // INSTALLATION
+        // Prepare installation data to create a new token
+        bytes memory installData;
+
+        {
+            MajorityVotingBase.VotingSettings memory votingSettings = MajorityVotingBase.VotingSettings(
+                MajorityVotingBase.VotingMode.EarlyExecution, 500_000, 200_000, 1 hours, 0
+            );
+            TokenVotingSetup.TokenSettings memory tokenSettings =
+                TokenVotingSetup.TokenSettings({addr: address(0), name: "New Token", symbol: "NEW"});
+            address[] memory receivers = new address[](1);
+            receivers[0] = alice;
+            uint256[] memory amounts = new uint256[](1);
+            amounts[0] = 1000 ether;
+            GovernanceERC20.MintSettings memory mintSettings = GovernanceERC20.MintSettings(receivers, amounts);
+            IPlugin.TargetConfig memory targetConfig = IPlugin.TargetConfig(address(dao), IPlugin.Operation.Call);
+
+            installData = abi.encode(votingSettings, tokenSettings, mintSettings, targetConfig, 0, "some-metadata");
+        }
+
+        // Prepare and apply the installation
+        address plugin;
+        IPluginSetup.PreparedSetupData memory preparedSetupData;
+
+        {
+            PluginSetupProcessor.PrepareInstallationParams memory prepareInstallParams =
+                PluginSetupProcessor.PrepareInstallationParams({pluginSetupRef: setupRef, data: installData});
+
+            (plugin, preparedSetupData) = pluginSetupProcessor.prepareInstallation(address(dao), prepareInstallParams);
+
+            PluginSetupProcessor.ApplyInstallationParams memory applyInstallParams = PluginSetupProcessor
+                .ApplyInstallationParams({
+                pluginSetupRef: setupRef,
+                plugin: plugin,
+                permissions: preparedSetupData.permissions,
+                helpersHash: hashHelpers(preparedSetupData.helpers)
+            });
+            pluginSetupProcessor.applyInstallation(plugin, applyInstallParams);
+
+            vm.assertTrue(
+                dao.isGranted(address(dao), address(plugin), dao.EXECUTE_PERMISSION_ID(), ""),
+                "Plugin should be installed"
+            );
+        }
+
+        // Assert that the installation created and configured the new token correctly
+        {
+            TokenVoting tokenVotingPlugin = TokenVoting(plugin);
+            IVotesUpgradeable newToken = tokenVotingPlugin.getVotingToken();
+            assertNotEq(address(newToken), address(0), "New token should have been created");
+            assertTrue(tokenVotingPlugin.isMember(alice), "Alice should be a member");
+            assertFalse(tokenVotingPlugin.isMember(bob), "Bob should not be a member");
+        }
+
+        // UNINSTALLATION
+        // Prepare and apply the uninstallation
+        {
+            PluginSetupProcessor.PrepareUninstallationParams memory uninstallParams = PluginSetupProcessor
+                .PrepareUninstallationParams({
+                pluginSetupRef: setupRef,
+                setupPayload: IPluginSetup.SetupPayload({
+                    plugin: plugin,
+                    currentHelpers: preparedSetupData.helpers,
+                    data: ""
+                })
+            });
+            PermissionLib.MultiTargetPermission[] memory uninstallPermissions =
+                pluginSetupProcessor.prepareUninstallation(address(dao), uninstallParams);
+
+            PluginSetupProcessor.ApplyUninstallationParams memory applyUninstallParams = PluginSetupProcessor
+                .ApplyUninstallationParams({plugin: plugin, pluginSetupRef: setupRef, permissions: uninstallPermissions});
+            pluginSetupProcessor.applyUninstallation(plugin, applyUninstallParams);
+        }
+
+        // Assert that the plugin was successfully uninstalled
+        vm.assertFalse(
+            dao.isGranted(address(dao), address(plugin), dao.EXECUTE_PERMISSION_ID(), ""),
+            "Plugin should be uninstalled"
+        );
     }
 }
--- test/lib/ForkTestBase.sol
+++ test/lib/ForkTestBase.sol
@@ -2,26 +2,32 @@
 pragma solidity ^0.8.17;
 
 import {Vm} from "forge-std/Test.sol";
-import {console2} from "forge-std/console2.sol";
+import {TestBase} from "./TestBase.sol";
 
 import {DAO} from "@aragon/osx/core/dao/DAO.sol";
 import {DAOFactory} from "@aragon/osx/framework/dao/DAOFactory.sol";
 import {PluginRepoFactory} from "@aragon/osx/framework/plugin/repo/PluginRepoFactory.sol";
 import {PluginRepo} from "@aragon/osx/framework/plugin/repo/PluginRepo.sol";
+import {PluginSetupProcessor} from "@aragon/osx/framework/plugin/setup/PluginSetupProcessor.sol";
 import {PluginSetupRef} from "@aragon/osx/framework/plugin/setup/PluginSetupProcessorHelpers.sol";
 
-import {TestBase} from "./TestBase.sol";
-
 contract ForkTestBase is TestBase {
-    DAOFactory internal immutable daoFactory = DAOFactory(vm.envOr("DAO_FACTORY_ADDRESS", address(0)));
+    // Defaults for OSx v1.4 on Sepolia
+    DAOFactory internal immutable daoFactory =
+        DAOFactory(vm.envOr("DAO_FACTORY_ADDRESS", address(0xB815791c233807D39b7430127975244B36C19C8e)));
     PluginRepoFactory internal immutable pluginRepoFactory =
-        PluginRepoFactory(vm.envOr("PLUGIN_REPO_FACTORY_ADDRESS", address(0)));
+        PluginRepoFactory(vm.envOr("PLUGIN_REPO_FACTORY_ADDRESS", address(0x399Ce2a71ef78bE6890EB628384dD09D4382a7f0)));
+    PluginSetupProcessor internal immutable pluginSetupProcessor = PluginSetupProcessor(
+        vm.envOr("PLUGIN_SETUP_PROCESSOR_ADDRESS", address(0xC24188a73dc09aA7C721f96Ad8857B469C01dC9f))
+    );
 
     function setUp() public virtual {
         if (address(daoFactory) == address(0)) {
             revert("Please, set DAO_FACTORY_ADDRESS on your .env file");
         } else if (address(pluginRepoFactory) == address(0)) {
             revert("Please, set PLUGIN_REPO_FACTORY_ADDRESS on your .env file");
+        } else if (address(pluginSetupProcessor) == address(0)) {
+            revert("Please, set PLUGIN_SETUP_PROCESSOR_ADDRESS on your .env file");
         }
 
         // Start the fork
