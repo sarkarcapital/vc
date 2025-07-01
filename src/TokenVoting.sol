9e0c6f9cc5113923ab9632c2aa7fef298037561b
Jør∂¡ <itsbrickpop@gmail.com>
2025-07-01 09:59:29 +0100

    Keeping the storage slot due to packing

--- src/TokenVoting.sol
+++ src/TokenVoting.sol
@@ -336,5 +336,5 @@ contract TokenVoting is IMembership, MajorityVotingBase {
     /// @dev This empty reserved space is put in place to allow future versions to add new
     /// variables without shifting down storage in the inheritance chain.
     /// https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-    uint256[48] private __gap;
+    uint256[49] private __gap;
 }
